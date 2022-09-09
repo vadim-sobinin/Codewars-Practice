0b081b8491356cbe961e1a2251a5bec2c9910822
Vadim Sobinin <103999720+vadim-sobinin@users.noreply.github.com>
2022-09-09 23:05:03 +0300

    kata 4 kyu Objectify a URL Query String

---
+++ Objectify-a-URL-Query-String.js
@@ -0,0 +1,29 @@
+function convertQueryToMap(query) {
+  if (!query) return {};
+  
+  const setObjProp = (source, prop, value) => {
+    let list = prop.split('.'),
+      obj = source;
+    
+    while (list.length > 1) {
+      let key = list.shift();
+      
+      if (!obj[key]) {        
+        obj[key] = {};
+      }
+      
+      obj = obj[key];
+    }
+    
+    obj[list.pop()] = decodeURIComponent(value);
+    
+    return source;
+  };
+  
+  return query.split('&')
+    .reduce((s, q) => {
+      let pieces = q.split('=');
+      s = setObjProp(s, pieces[0], pieces[1]);
+      return s;
+    }, {}); 
+}
