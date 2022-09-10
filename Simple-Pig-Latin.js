30be9d4d60224a93355b93545b24f230161738db
Vadim Sobinin <103999720+vadim-sobinin@users.noreply.github.com>
2022-09-10 13:09:59 +0300

    5 kyu Simple Pig Latin

---
+++ Simple-Pig-Latin.js
@@ -0,0 +1,15 @@
+//5 kyu Simple Pig Latin
+
+function pigIt(str){
+  let strArr = str.split(' ');
+  let pigLatin = [];
+
+  for(let word of strArr){
+    if((/([a-zA-Z])/).test(word)){
+      pigLatin.push(word.substring(1) + word[0] + "ay");
+    }else{
+      pigLatin.push(word);
+    }
+  }
+  return pigLatin.join(" ");
+}
