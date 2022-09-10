c1c84980e29b1ac1d3c9525da082e63b058d82ec
Vadim Sobinin <103999720+vadim-sobinin@users.noreply.github.com>
2022-09-10 11:49:09 +0300

    Valid Parentheses

---
+++ Valid-Parentheses.js
@@ -0,0 +1,12 @@
+// 5 kyu Valid Parentheses
+
+function validParentheses(parens){
+  var n = 0;
+  for (var i = 0; i < parens.length; i++) {
+    if (parens[i] == '(') n++;
+    if (parens[i] == ')') n--;
+    if (n < 0) return false;
+  }
+  
+  return n == 0;
+}
